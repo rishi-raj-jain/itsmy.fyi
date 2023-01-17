66fad187b186fd6e0ae3c7446f959ad88974b4c2
Rishi Raj Jain <rjain@llnw.com>
2023-01-17 14:40:35 +0530

    move to yarn

--- src/api.js
+++ src/api.js
@@ -8,10 +8,12 @@ export async function getUserItems(slug) {
   let resp
   if (import.meta.env.GITHUB_API_TOKEN) {
     console.log(JSON.stringify(import.meta.env.GITHUB_API_TOKEN))
+    // ex. https://api.github.com/repos/rishi-raj-jain/itsyour.page/contents/jsons%2Frishi-raj-jain.json?ref=main
     resp = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}{?ref}', {
       owner: 'rishi-raj-jain',
       repo: 'itsyour.page',
       path: 'jsons/' + slug + '.json',
+      ref: 'main'
     })
   } else {
     // Define 1-click logic
