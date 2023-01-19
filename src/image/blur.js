133a9d8fcb93789b3d24b91ff726a40f1f74e7e6
Rishi Raj Jain <rjain@llnw.com>
2023-01-19 07:27:11 +0530

    Generate blur on fly

---
+++ src/image/blur.js
@@ -0,0 +1,20 @@
+import imagemin from 'imagemin'
+import imageminJpegtran from 'imagemin-jpegtran'
+
+export async function getBase64ImageUrl(imageUrl, width = 50) {
+  try {
+    if (imageUrl.startsWith('https:')) {
+      const response = await fetch(`https://opt.moovweb.net/?img=${imageUrl}&width=${width}`)
+      const buffer = await response.arrayBuffer()
+      const minified = await imagemin.buffer(Buffer.from(buffer), {
+        plugins: [imageminJpegtran({ progressive: true })],
+      })
+      const base64 = Buffer.from(minified).toString('base64')
+      return { data: `data:image/jpeg;base64,${base64}` }
+    }
+    return {}
+  } catch (e) {
+    console.log(e.message || e.toString())
+    return {}
+  }
+}
