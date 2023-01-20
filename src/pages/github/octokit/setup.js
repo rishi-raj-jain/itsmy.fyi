ec0b59c0cae0a8fc4f486e9894ae164c7e8c70ff
Rishi Raj Jain <rjain@llnw.com>
2023-01-20 18:09:19 +0530

    Switch based on meta

--- src/pages/github/octokit/setup.js
+++ src/pages/github/octokit/setup.js
@@ -1,5 +1,11 @@
 import { Octokit } from '@octokit/rest'
 
-const auth = import.meta.env.GITHUB_API_TOKEN ?? process.env.GITHUB_API_TOKEN
+let auth
+
+if (import.meta && import.meta.env) {
+  auth = import.meta.env.GITHUB_API_TOKEN
+} else if (process.env) {
+  auth = process.env.GITHUB_API_TOKEN
+}
 
 export const octokit = new Octokit({ auth })
