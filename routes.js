762c4ef592149ff473cde91f390e5e13f9ae2019
Rishi Raj Jain <rjain@llnw.com>
2023-01-20 19:34:20 +0530

    add open source div

--- routes.js
+++ routes.js
@@ -47,7 +47,35 @@ const optimizePage = ({ cache, removeUpstreamResponseHeader, renderWithApp }) =>
 const router = new Router({ indexPermalink: true })
 
 // Home page
-router.match('/', optimizePage)
+router.match('/', ({ cache, removeUpstreamResponseHeader, renderWithApp }) => {
+  removeUpstreamResponseHeader('cache-control')
+  cache({
+    edge: {
+      maxAgeSeconds: 60 * 60 * 24,
+      staleWhileRevalidateSeconds: 60 * 60 * 24 * 365,
+    },
+    browser: false,
+    key: new CustomCacheKey().excludeAllQueryParameters(),
+  })
+  renderWithApp({
+    transformResponse: async (res, req) => {
+      let statusCodeOriginal = res.statusCode
+      try {
+        if (res.getHeader('content-type').includes('/html')) {
+          const $ = load(res.body)
+          const { minify } = await esImport('html-minifier')
+          res.body = minify($.html(), minifyOptions)
+        } else if (res.getHeader('content-type').includes('/xml')) {
+          const { minify } = await esImport('html-minifier')
+          res.body = minify(res.body.toString(), minifyOptions)
+        }
+      } catch (e) {
+        console.log(e.message || e.toString())
+        res.statusCode = statusCodeOriginal
+      }
+    },
+  })
+})
 
 // Sitemap
 router.match('/sitemap.xml', optimizePage)
