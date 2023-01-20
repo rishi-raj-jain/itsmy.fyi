7dcb0123ddc435bb455d03ab53136a2cf1c72206
Rishi Raj Jain <rjain@llnw.com>
2023-01-20 15:14:48 +0530

    redirect to the correct param

--- routes.js
+++ routes.js
@@ -76,7 +76,7 @@ router.match('/me/:slug', ({ compute, redirect, send }) => {
       }
       console.log(JSON.stringify(analyticsObject))
       req.setHeader('ITS-MY-PROTECTION', process.env.GITHUB_WEBHOOK_SECRET)
-      return redirect('/u/:path')
+      return redirect('/u/:slug')
     } else {
       send('Invalid Request', 403)
     }
