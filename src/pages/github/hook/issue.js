8f4da49db0bc9700b0fed8861c367a130f47a62a
Rishi Raj Jain <rjain@llnw.com>
2023-01-21 03:20:19 +0530

    cleaner way to put error messages

--- src/pages/github/hook/issue.js
+++ src/pages/github/hook/issue.js
@@ -39,11 +39,16 @@ export async function post({ request }) {
   const data = validateEvent(context)
 
   if (data.error) {
+    const errorMessage = [
+      "Bad luck! Here's what we found breaking in your data:\n\n",
+      '```markdown\n' + data.error + '\n```',
+      "\n\nI promise I'll handle this better, but you'd have to create a [new issue](https://github.com/rishi-raj-jain/itsmy.fyi/issues/new?assignees=&labels=&template=itsyour.page-profile-data.yml&title=itsmy.fyi+-+%7BINSERT+NAME%7D+%28Optional%29) for now.",
+    ]
     await octokit.rest.issues.createComment({
       owner: context.repository.owner.login,
       repo: context.repository.name,
       issue_number: data.issue,
-      body: `Bad luck! Here's what we found breaking in your data:\n\`\`\`markdown\n${data.error}\n\`\`\`.\n\nI promise I'll handle this better, but you'd have to create a [new issue](https://github.com/rishi-raj-jain/itsmy.fyi/issues/new?assignees=&labels=&template=itsyour.page-profile-data.yml&title=itsmy.fyi+-+%7BINSERT+NAME%7D+%28Optional%29) for now.`,
+      body: errorMessage.join('').toString(),
     })
     return
   }
