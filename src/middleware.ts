9ae61fb39f25334d4bf24c0f96e0a551bbfff396
Rishi Raj Jain <rjain@llnw.com>
2023-07-03 16:03:57 +0530

    explicitly return the next response

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,19 @@
 import { getENV } from '@/lib/env'
+import { defineMiddleware } from 'astro/middleware'
 
-export function onRequest({ request }) {
-  if (request.headers.get('edgio') !== getENV('EDGIO_HEADER')) {
+/**
+ * @type {import("astro").MiddlewareResponseHandler}
+ */
+// `context` and `next` are automatically typed
+export const onRequest = defineMiddleware(async (context, next) => {
+  if (context.request.headers.get('edgio') !== getENV('EDGIO_HEADER')) {
     return new Response(null, {
       status: 403,
     })
   }
-}
+  const response = await next()
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers,
+  })
+})
