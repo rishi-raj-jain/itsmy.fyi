d2fc0647fe98ca3077f8193cbd7dc58a10a6bb4e
Rishi Raj Jain <rjain@llnw.com>
2023-07-25 23:04:21 +0530

    upgrade astro to the latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
+  build: {
+    split: true,
+  },
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind()],
