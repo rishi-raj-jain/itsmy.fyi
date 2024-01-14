bb5718968d85749234cf54488d4ef9a1f8a5548e
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-14 19:44:19 +0530

    remove middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import vercel from '@astrojs/vercel/serverless'
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    edgeMiddleware: true,
     functionPerRoute: true,
   }),
   integrations: [tailwind()],
