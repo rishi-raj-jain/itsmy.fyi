d14891f11e2244f240105154ae058c59bab6528c
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-04-08 17:24:51 +0530

    default

--- lib/Upstash/setup.ts
+++ lib/Upstash/setup.ts
@@ -1,9 +1,7 @@
 import { getENV } from '../env'
 import { Redis } from '@upstash/redis'
 
-const redis = new Redis({
+export default new Redis({
   url: getENV('UPSTASH_REDIS_REST_URL'),
   token: getENV('UPSTASH_REDIS_REST_TOKEN'),
 })
-
-export default redis
